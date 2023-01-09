2f0f40e18b7694b530a7d8e5abf82285d0b90f73
xbt573 <64583541+xbt573@users.noreply.github.com>
2023-01-09 14:24:10 +0300

    nvim, sway, waybar, mako changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,18 @@
-vim.o.compatible    = false
+vim.o.compatible = false
 
-vim.o.number        = true
-vim.o.tabstop       = 4
-vim.o.shiftwidth    = 4
-vim.o.expandtab     = true
+vim.o.number     = true
+vim.o.tabstop    = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab  = true
 
-vim.o.foldmethod    = 'indent'
-vim.o.foldnestmax   = 10
-vim.o.foldenable    = false
-vim.o.foldlevel     = 2
+vim.o.foldmethod  = 'indent'
+vim.o.foldnestmax = 10
+vim.o.foldenable  = false
+vim.o.foldlevel   = 2
 
-vim.o.clipboard     = 'unnamedplus'
-vim.o.showmode      = false
-vim.o.splitright    = true
+vim.o.clipboard  = 'unnamedplus'
+vim.o.showmode   = false
+vim.o.splitright = true
 
 vim.o.termguicolors = true
 vim.o.completeopt   = 'menu,menuone,noselect'
@@ -24,183 +24,319 @@ vim.o.gdefault = true
 vim.o.swapfile = false
 
 vim.diagnostic.config({
-	update_in_insert = true
+    update_in_insert = true
 })
 
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+    pattern = { '*.go', '*.lua' },
+    callback = function()
+        vim.lsp.buf.formatting_sync(nil, 3000)
+    end
+})
+
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map("i", "jk", "<ESC><ESC>", { silent = true })
+
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-		vim.cmd [[packadd packer.nvim]]
-		return true
-	end
-	return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'ellisonleao/gruvbox.nvim',
-		config = function()
-			require('gruvbox').setup {
-				italic = false,
-				contrast = "hard",
-			}
-			vim.cmd('colorscheme gruvbox')
-		end
-	}
+    use {
+        'xbt573/gruvbox.nvim',
+        config = function()
+            require('gruvbox').setup {
+                italic = false,
+                contrast = "hard",
+                transparent_mode = true,
+            }
+            vim.cmd('colorscheme gruvbox')
+        end
+    }
 
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = {
-			'kyazdani42/nvim-web-devicons',
-			opt = true
-		},
-		config = function()
-			require('lualine').setup({})
-		end
-	}
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {
+            -- { 'kyazdani42/nvim-web-devicons', opt = true },
+            -- { 'SmiteshP/nvim-navic', opt = true },
+            'kyazdani42/nvim-web-devicons',
+            'SmiteshP/nvim-navic'
+        },
+        config = function()
+            require('lualine').setup({
+                sections = {
+                    lualine_c = {
+                        { require('nvim-navic').get_location, cond = require('nvim-navic').is_available },
+                    }
+                }
+            })
+        end
+    }
 
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-			ts_update()
-		end,
-		config = function()
-			require('nvim-treesitter.configs').setup {
-				ensure_installed = { 'lua', 'cpp', 'c', 'kotlin', 'go', 'python' },
-				highlight = {
-					enable = true
-				},
-				indent = {
-					enable = true
-				}
-			}
-		end
-	}
-	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = { 'nvim-tree/nvim-web-devicons' },
-		config = function()
-			vim.g.loaded_netrw = 1
-			vim.g.loaded_netrwPlugin = 1
-			require("nvim-tree").setup()
-			vim.api.nvim_set_keymap('', '<C-r>', '<ESC>:NvimTreeToggle<CR>', { noremap = true, silent = true})
-		end
-	}
-	use {
-		'neovim/nvim-lspconfig',
-		requires = {
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-cmdline',
-			'hrsh7th/nvim-cmp',
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = { 'lua', 'cpp', 'c', 'kotlin', 'go', 'python' },
+                highlight = {
+                    enable = true
+                },
+                indent = {
+                    enable = true
+                }
+            }
+        end
+    }
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+            require("nvim-tree").setup {
+                auto_reload_on_write = true,
+                diagnostics = {
+                    enable = true
+                }
+            }
+            vim.api.nvim_set_keymap('', '<C-r>', '<ESC>:NvimTreeToggle<CR>', { noremap = true, silent = true })
+        end
+    }
+    use {
+        'neovim/nvim-lspconfig',
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/nvim-cmp',
 
-			'hrsh7th/vim-vsnip',
-			'hrsh7th/cmp-vsnip',
+            'hrsh7th/vim-vsnip',
+            'hrsh7th/cmp-vsnip',
 
-			'williamboman/mason.nvim',
-			'williamboman/mason-lspconfig.nvim'
-		},
-		config = function()
-			require("mason").setup()
-			require("mason-lspconfig").setup()
-			require("mason-lspconfig").setup_handlers {
-				function (server_name)
-					require("lspconfig")[server_name].setup {}
-				end,
-			}
-			local cmp = require('cmp')
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						vim.fn["vsnip#anonymous"](args.body)
-					end
-				},
-				window = {
-					documentation = cmp.config.window.bordered(),
-					completion = cmp.config.window.bordered()
-				},
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
 
-				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }),
-				}),
+            'SmiteshP/nvim-navic'
+        },
+        config = function()
+            -- Mappings.
+            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+            local opts = { noremap = true, silent = true }
+            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-				sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = 'vsnip' }
-				}, {
-					{ name = 'buffer' },
-				})
-			})
+            -- Use an on_attach function to only map the following keys
+            -- after the language server attaches to the current buffer
+            local on_attach = function(client, bufnr)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-			cmp.setup.filetype('gitcommit', {
-				sources = cmp.config.sources({
-					{ name = 'cmp_git' },
-				}, {
-					{ name = 'buffer' },
-				})
-			})
+                -- Mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                local bufopts = { noremap = true, silent = true, buffer = bufnr }
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+                vim.keymap.set('n', '<space>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, bufopts)
+                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+                vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+                vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+            end
 
-			cmp.setup.cmdline({ '/', '?' }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = 'buffer' }
-				}
-			})
+            require("mason").setup()
+            require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup_handlers {
+                function(server_name)
+                    require("lspconfig")[server_name].setup {
+                        on_attach = on_attach
+                    }
+                end,
 
-			cmp.setup.cmdline(':', {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = 'path' }
-				}, {
-					{ name = 'cmdline' }
+                ["gopls"] = function()
+                    require('lspconfig').gopls.setup {
+                        on_attach = function(c, b)
+                            on_attach(c, b)
+                            require('inlay-hints').on_attach(c, b)
+                            require('nvim-navic').attach(c, b)
+                        end,
+                        settings = {
+                            gopls = {
+                                hints = {
+                                    assignVariableTypes = true,
+                                    compositeLiteralFields = true,
+                                    constantValues = true,
+                                    parameterNames = true,
+                                    rangeVariableTypes = true,
+                                }
+                            }
+                        }
+                    }
+                end,
 
-				})
-			})
+                ["sumneko_lua"] = function()
+                    require 'lspconfig'.sumneko_lua.setup {
+                        on_attach = on_attach,
+                        settings = {
+                            Lua = {
+                                runtime = {
+                                    version = 'LuaJIT',
+                                },
+                                diagnostics = {
+                                    globals = { 'vim' },
+                                },
+                                workspace = {
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                },
+                                telemetry = {
+                                    enable = false,
+                                },
+                            },
+                        },
+                    }
+                end
+            }
+            local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end
+                },
+                window = {
+                    documentation = cmp.config.window.bordered(),
+                    completion = cmp.config.window.bordered()
+                },
 
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                }),
 
-			local lspconfig = require('lspconfig')
-			lspconfig.util.default_config = vim.tbl_extend(
-			"force",
-			lspconfig.util.default_config,
-			{ capabilities = capabilities }
-			)
-		end
-	}
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'vsnip' }
+                }, {
+                    { name = 'buffer' },
+                })
+            })
+
+            cmp.setup.filetype('gitcommit', {
+                sources = cmp.config.sources({
+                    { name = 'cmp_git' },
+                }, {
+                    { name = 'buffer' },
+                })
+            })
+
+            cmp.setup.cmdline({ '/', '?' }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    { name = 'cmdline' }
+
+                })
+            })
+
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local lspconfig = require('lspconfig')
+            lspconfig.util.default_config = vim.tbl_extend(
+                "force",
+                lspconfig.util.default_config,
+                { capabilities = capabilities }
+            )
+        end
+    }
 
     use {
         'akinsho/bufferline.nvim',
         tag = "v3.*",
-        requires = { 'nvim-tree/nvim-web-devicons' },
+        requires = 'nvim-tree/nvim-web-devicons',
         config = function()
-            diagnostics_indicator = function(count, level, diagnostics_dict, context)
-                local icon = level:match("error") and " " or " "
-                return " " .. icon .. count
-            end
-
-            require("bufferline").setup{
+            require("bufferline").setup {
                 options = {
-                    diagnostics = "nvim_lsp",
-                    diagnostics_update_in_insert = true,
-                    diagnostics_indicator = diagnostics_indicator
+                    mode = "tabs",
+                    numbers = "buffer_id",
+                    offsets = {
+                        {
+                            filetype = "NvimTree",
+                            text = "Files",
+                            text_align = "center",
+                            separator = true
+                        }
+                    },
+                    color_icons = true
                 }
             }
         end
     }
 
-	if packer_bootstrap then
-		require('packer').sync()
-	end
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup()
+        end
+    }
+
+    use 'tpope/vim-commentary'
+    -- use {
+    --     'lvimuser/lsp-inlayhints.nvim',
+    --     config = function()
+    --         require('lsp-inlayhints').setup()
+    --     end
+    -- }
+    use {
+        'simrat39/inlay-hints.nvim',
+        config = function()
+            require("inlay-hints").setup()
+        end
+    }
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
